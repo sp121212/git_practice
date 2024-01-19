90183d80a3d75a5d05c100c6b14204fb398e7dd5
santanu <spanda@cloudlex.com>
2024-01-19 07:58:56 +0530

    added/modified/deleted files

--- src/pages/LoginPage.java
+++ src/pages/LoginPage.java
@@ -5,7 +5,14 @@ public class LoginPage {
 	
 	public static void main(String[] args) {
 		
-		System.out.println("Login page");
+		System.out.println("Hello Login page");
+		
+		
+		String un="admin";
+		String pw="admin";
+		
+		System.out.println("User Logged In: "+ un +" -- " +pw);
+		
 		
 	}
 }
---
+++ src/pages/SearchPage.java
@@ -0,0 +1,11 @@
+package pages;
+
+public class SearchPage {
+	
+	public static void main(String[] args) {
+		System.out.println("Search");
+		
+		
+	}
+
+}
